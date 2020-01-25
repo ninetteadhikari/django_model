db25adc4a9680724fa4b1ff1158b58b9727a0ed8
Ninette <ninette.adhikari@gmail.com>
2020-01-25 12:26:33 +0100

    initial commit

---
+++ task2_django/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Groups (models.Model):
+    
+
+class Users (models.Model):
+class Posts (models.Model):
+class Comments (models.Model):
+class Replies (models.Model):
