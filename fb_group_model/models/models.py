2606d8e0a71dd6e2fa2bf2c426116d68a4e2a18c
Ninette <ninette.adhikari@gmail.com>
2020-01-27 21:41:26 +0100

    reply model update

--- fb_group_model/models/models.py
+++ fb_group_model/models/models.py
@@ -7,7 +7,7 @@ class Groups (models.Model):
     cover_photo=models.ImageField(upload_to='coverphoto')
     icon=models.ImageField(upload_to='iconphoto')
     member_count=models.IntegerField()
-    users = models.ManyToManyField('models.Users',blank=True)
+    members = models.ManyToManyField('models.Users',blank=True)
 
     def __str__(self):
         return self.group_name
@@ -48,24 +48,18 @@ class Posts (models.Model):
 class Likes (models.Model):
     likes=models.BooleanField()
     post=models.ForeignKey(Posts, on_delete=models.CASCADE)
+    comments=models.ForeignKey('models.Comments', on_delete=models.CASCADE)
     user=models.ForeignKey(Users, null=True,on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.likes
 
 class Comments (models.Model):
-    comment_description=models.TextField()
+    description=models.TextField()
     created_date=models.DateTimeField()
     post=models.ForeignKey(Posts, blank=True, on_delete=models.CASCADE)
+    user=models.ForeignKey(Users, null=True, on_delete=models.SET_NULL)
+    reply=models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return self.comment_description
-
-
-class Replies (models.Model):
-    reply_description=models.TextField()
-    created_date=models.DateTimeField()
-    comment=models.ForeignKey(Comments,blank=True,on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.reply_description
+        return self.description
